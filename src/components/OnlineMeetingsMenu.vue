e7407171f8d2d84d24c25569be5c253338bed0d7
Ebrar Karademir <ebrarcode@hotmail.com>
2024-05-30 21:50:28 +0300

    commit

--- src/components/OnlineMeetingsMenu.vue
+++ src/components/OnlineMeetingsMenu.vue
@@ -1,7 +1,9 @@
 <template>
     <section class="min-h-[88vh] flex items-center justify-center">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            <button class="bg-white rounded-lg border w-64 h-64 border-gray-200 shadow-lg flex items-center justify-center">
+            <button 
+            @click="toggleShowAll"
+            class="bg-white rounded-lg border w-64 h-64 border-gray-200 shadow-lg flex items-center justify-center">
                 <div class="flex flex-col items-center">
                     <div class="text-[1.6rem] font-medium text-gray-500">Tüm Görüşmeleri</div>
                     <div class="text-[1.6rem] font-medium text-gray-500">Gör</div>
@@ -37,6 +39,13 @@
 
 <script setup>
 
+import { defineEmits } from 'vue';
+
+const emit = defineEmits(['toggle-show-all']);
+
+const toggleShowAll = () => {
+  emit('toggle-show-all');
+};
 
 </script>
 
--- src/views/OnlineMeetings.vue
+++ src/views/OnlineMeetings.vue
@@ -1,9 +1,11 @@
 <template>
   <OnlineMeetingsMenu 
-    v-if="userRole === 'admin'"
+    v-if="userRole === 'admin' && !showAll"
+    @toggle-show-all="handleToggleShowAll"
+
   />
   <OnlineMeetingsManagement 
-    v-else-if="userRole === 'attorney'"
+    v-else-if="userRole === 'attorney' || showAll"
     :userRole="userRole"
     :userUid="userUid"
   />
@@ -16,12 +18,20 @@ import OnlineMeetingsMenu from '../components/OnlineMeetingsMenu.vue';
 import OnlineMeetingsManagement from '../components/OnlineMeetingsManagement.vue';
 import { computed } from 'vue';
 import { useStore } from 'vuex';
+import { ref } from 'vue';
+
+const showAll = ref(false);
+
 // Access the Vuex store
 const store = useStore();
 
 // Create computed properties for the user role and UID
 const userRole = computed(() => store.getters.userRole);
 const userUid = computed(() => store.getters.user ? store.getters.user.uid : null);
+
+const handleToggleShowAll = () => {
+  showAll.value = !showAll.value;
+};
 </script>
 
 
